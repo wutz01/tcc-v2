5d40233806a96c56be6b268be08e931488cc083e
Ben <realben10nnyson>
2018-12-18 06:05:44 +0800

    Update

--- Admin/addGradesApplicant.php
+++ Admin/addGradesApplicant.php
@@ -1,142 +1,143 @@
-<?php
-  require '../Database/database2.php';
-  require '../Database/database.php';
-  include_once "../General/header.php";
-  include_once "../General/topBar.php";
-  include_once "../General/leftSideBar.php";
-  include_once "adminClass.php";
-
-  $admin = new Admin();
-  if(isset($_GET["id"])) {
-    $id = $_GET["id"];
-    $query = "SELECT * FROM tbl_applicant WHERE fld_applicantID = '$id'";
-    $res = mysqli_query($conn, $query);
-    $user = mysqli_fetch_assoc($res);
-  }
-?>
-
-<script type="text/javascript">
-  $('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
-</script>
-<div class="page animsition">
-   <div class="page-header">
-      <h1 class="page-title">Admin</h1>
-      <ol class="breadcrumb">
-         <li><a href="../General/dashboard.php">Home</a></li>
-         <li class="active">Add Grades</li>
-      </ol>
-   </div>
-  <div class="page-content">
-    <div class="panel">
-      <div class="panel-body container-fluid">
-        <div class="row row-lg">
-          <div class="col-sm-12">
-            <div class="col-sm-12">
-              <div class="example-wrap">
-              <h1><?php echo $user['fld_lastName'] ?>, <?php echo $user['fld_firstName'] ?> <?php echo $user['fld_middleName'] ?></h1>
-                <div class="example">
-                  <div class="form-group row">
-                    <form method="post" id="gradeApplicantSubmit" action="addGradesApplicantAPI.php">
-                      <center><table id="example" style="width: 50%">
-                            <tr>
-                               <th>Subject Name</th>
-                               <th>Grade</th>
-                            </tr>
-                            <tr>
-                              <td colspan="2">
-                                <input type="text" name="applicantUserID" id="applicantUserID" value="<?php echo $user['fld_applicantID'] ?>" hidden />
-                                <select id="subjectNameSelect" class='form-control'>
-                                  <?php $getApplicantSubject = $admin->getApplicantSubjectNotDefault();
-                                    while($row = $getApplicantSubject->fetch(PDO::FETCH_ASSOC)){
-                                    extract($row);
-                                  ?>
-                                  <option value="<?php echo $id ?>" data-id="<?php echo $id ?>"><?php echo $fld_subject ?></option>
-                                  <?php } ?>
-                                </select>
-                              </td>
-                              <td>
-                                <button class="btn btn-primary" onclick="addRow()" type="button">+</button>
-                              </td>
-                            </tr>
-                          <?php
-                            $getApplicantSubject = $admin->getApplicantSubjectDefault();
-                            while($row = $getApplicantSubject->fetch(PDO::FETCH_ASSOC)){
-                            extract($row);
-                            $userId = $_GET["id"];
-                            $queryHave = "SELECT * FROM tbl_grades_applicant WHERE fld_applicantID = '$userId' AND fld_subjectID = $id";
-                            $result = mysqli_query($conn, $queryHave);
-                            $userHave = mysqli_fetch_assoc($result);
-                          ?>
-                            <tr>
-                               <td><input type="text" name="subjectName[<?php echo $id ?>]" id="subjectName" value="<?php echo $id; ?>" hidden><?php echo $fld_subject; ?></td>
-                               <td><input type="number" class="form-control" name="grade[<?php echo $id ?>]" id="grade" placeholder="Grade" value="<?php echo $userHave['fld_grade'] ?>"/></td>
-                            </tr>
-                          <?php
-                            }
-                          ?>
-                      </table>
-                      </center>
-                      <button class="btn btn-success pull-right" name="validate" type="submit">Save</button>
-                      <a href="acceptedApplicantProfile.php?id=<?php echo $_GET["id"] ?>"><button class="btn btn-danger">Cancel</button></a>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-</div>
-<!-- End Page -->
-<?php
-   include_once "../General/footer.php";
-?>
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script> -->
-<script src="../assets/plugins/jquery-form/jquery-form.min.js"></script>
-<!-- <script src="../OSPI/assets/js/jquery-3.1.1.min.js"></script> -->
-
-<script>
-  function addRow() {
-    let subject = $("#subjectNameSelect").val();
-    let subjectList = $("#subjectNameSelect option:selected").text();
-    let html = '';
-    html += '<tr><td><input  type="text" name="subjectName[<?php echo $id ?>]" id="subjectName" hidden>' +subjectList+ '</td>' +
-      '<td><input type="number" class="form-control" name="grade[]" id="grade" placeholder="Grade" /></td><td><button class="btn btn-danger" type="button" onclick="removeRow()">-</button></tr>';
-    $('#example').find('tbody').append(html);
-  }
-  // function removeRow(){
-
-  // }
-</script>
-
-<script type="text/javascript">
-  $(function () {
-    $('#gradeApplicantSubmit').ajaxForm({
-      dataType: 'json',
-      success: (o) => {
-        if(o.success){
-          alert(o.message)
-          location.reload();
-        } else {
-          alert('error')
-        }
-      },
-      beforeSubmit: (o) => {
-      }
-    });
-  })
-</script>
-<link rel="stylesheet" type="text/css" href="../assets/js/datatables.min.css"/>
-<script type="text/javascript" src="../assets/js/datatables.min.js"></script>
-<script type="text/javascript">
-  $(document).ready( function () {
-    $('#example').DataTable();
-} );
+<?php
+  require '../Database/database2.php';
+  require '../Database/database.php';
+  include_once "../General/header.php";
+  include_once "../General/topBar.php";
+  include_once "../General/leftSideBar.php";
+  include_once "adminClass.php";
+
+  $admin = new Admin();
+  if(isset($_GET["id"])) {
+    $id = $_GET["id"];
+    $query = "SELECT * FROM tbl_applicant WHERE fld_applicantID = '$id'";
+    $res = mysqli_query($conn, $query);
+    $user = mysqli_fetch_assoc($res);
+  }
+?>
+
+<script type="text/javascript">
+  $('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+</script>
+<div class="page animsition">
+   <div class="page-header">
+      <h1 class="page-title">Admin</h1>
+      <ol class="breadcrumb">
+         <li><a href="../General/dashboard.php">Home</a></li>
+         <li class="active">Add Grades</li>
+      </ol>
+   </div>
+  <div class="page-content">
+    <div class="panel">
+      <div class="panel-body container-fluid">
+        <div class="row row-lg">
+          <div class="col-sm-12">
+            <div class="col-sm-12">
+              <div class="example-wrap">
+              <h1><?php echo $user['fld_lastName'] ?>, <?php echo $user['fld_firstName'] ?> <?php echo $user['fld_middleName'] ?></h1>
+                <div class="example">
+                  <div class="form-group row">
+                    <form method="post" id="gradeApplicantSubmit" action="addGradesApplicantAPI.php">
+                      <center><table id="example" style="width: 50%">
+                            <tr>
+                               <th>Subject Name</th>
+                               <th>Grade</th>
+                            </tr>
+                            <tr>
+                              <td colspan="2">
+                                <input type="text" name="applicantUserID" id="applicantUserID" value="<?php echo $user['fld_applicantID'] ?>" hidden />
+                                <select id="subjectNameSelect" class='form-control'>
+                                  <?php $getApplicantSubject = $admin->getApplicantSubjectNotDefault();
+                                    while($row = $getApplicantSubject->fetch(PDO::FETCH_ASSOC)){
+                                    extract($row);
+                                  ?>
+                                  <option value="<?php echo $id ?>" data-id="<?php echo $id ?>"><?php echo $fld_subject ?></option>
+                                  <?php } ?>
+                                </select>
+                              </td>
+                              <td>
+                                <button class="btn btn-primary" onclick="addRow()" type="button">+</button>
+                              </td>
+                            </tr>
+                          <?php
+                            $getApplicantSubject = $admin->getApplicantSubjectDefault();
+                            while($row = $getApplicantSubject->fetch(PDO::FETCH_ASSOC)){
+                            extract($row);
+                            $userId = $_GET["id"];
+                            $queryHave = "SELECT * FROM tbl_grades_applicant WHERE fld_applicantID = '$userId' AND fld_subjectID = $id";
+                            $result = mysqli_query($conn, $queryHave);
+                            $userHave = mysqli_fetch_assoc($result);
+                          ?>
+                            <tr>
+                               <td><input type="text" name="subjectName[<?php echo $id ?>]" id="subjectName" value="<?php echo $id; ?>" hidden><?php echo $fld_subject; ?></td>
+                               <td><input type="number" class="form-control" name="grade[<?php echo $id ?>]" id="grade" placeholder="Grade" value="<?php echo $userHave['fld_grade'] ?>"/></td>
+                            </tr>
+                          <?php
+                            }
+                          ?>
+                      </table>
+                      </center>
+                      <button class="btn btn-success pull-right" name="validate" type="submit">Save</button> 
+                      <a href="acceptedApplicantProfile.php?id=<?php echo $_GET["id"] ?>"><button class="btn btn-danger">Cancel</button></a>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+<!-- End Page -->
+<?php
+   include_once "../General/footer.php";
+?>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script> -->
+<script src="../OSPI/assets/js/jquery.form.min.js"></script>
+<!-- <script src="../OSPI/assets/js/jquery-3.1.1.min.js"></script> -->
+
+<script>
+  function addRow() {
+    let subject = $("#subjectNameSelect").val();
+    let subjectList = $("#subjectNameSelect option:selected").text();
+    let html = '';
+    html += '<tr><td><input  type="text" name="subjectName[<?php echo $id ?>]" id="subjectName" hidden>' +subjectList+ '</td>' + 
+      '<td><input type="number" class="form-control" name="grade[]" id="grade" placeholder="Grade" /></td><td><button class="btn btn-danger" type="button" onclick="removeRow()">-</button></tr>';
+    $('#example').find('tbody').append(html);
+  }
+  // function removeRow(){
+
+  // }
+</script>
+
+<script type="text/javascript">
+  $(function () {
+    $('#gradeApplicantSubmit').ajaxForm({
+      dataType: 'json',
+      success: (o) => {
+        if(o.success){
+          alert(o.message)
+          location.reload();
+        } else {
+          alert('error')
+        }
+      },
+      beforeSubmit: (o) => {
+        alert('Do you want to save?');
+      }
+    });
+  })
+</script>
+<link rel="stylesheet" type="text/css" href="../assets/js/datatables.min.css"/> 
+<script type="text/javascript" src="../assets/js/datatables.min.js"></script>
+<script type="text/javascript">
+  $(document).ready( function () {
+    $('#example').DataTable();
+} );
 </script>
--- Admin/addGradesApplicantAPI.php
+++ Admin/addGradesApplicantAPI.php
@@ -1,36 +1,36 @@
-<?php
-	session_start();
-	require '../Database/database2.php';
-
-	// print_r($_POST);
-	// die();
-
-	$applicantID = $_POST['applicantUserID'];
-
-	foreach($_POST['grade'] as $subjectId => $grade) {
-		if ((!isset($grade) || $grade === '')) {
-			$json['success'] = true;
-			$json['message'] = "Enter grade.";
-			echo json_encode($json, 200);
-			exit();
-		}	else {
-			$searchQuery = "SELECT * FROM tbl_grades_applicant WHERE fld_applicantID = $applicantID AND fld_subjectID = $subjectId";
-			$ret = mysqli_query($conn, $searchQuery);
-			$sub = mysqli_fetch_assoc($ret);
-			if ($sub) {
-				$id = $sub['id'];
-				$query = "UPDATE tbl_grades_applicant SET fld_applicantID = '$applicantID' , fld_subjectID = '$subjectId', fld_grade = '$grade' WHERE id = $id";
-			} else {
-				$query = "INSERT INTO tbl_grades_applicant (fld_applicantID, fld_subjectID, fld_grade) VALUES ('$applicantID', '$subjectId', '$grade')";	
-			}
-			
-		$test = mysqli_query($conn, $query);
-		}
-		
-	}
-	
-	$json['success'] = true;
-	$json['message'] = "Grades has been saved.";
-	echo json_encode($json, 200);
-	exit();
+<?php
+	session_start();
+	require '../Database/database2.php';
+
+	// print_r($_POST);
+	// die();
+
+	$applicantID = $_POST['applicantUserID'];
+
+	foreach($_POST['grade'] as $subjectId => $grade) {
+		if ((!isset($grade) || $grade === '')) {
+			$json['success'] = true;
+			$json['message'] = "Enter grade.";
+			echo json_encode($json, 200);
+			exit();
+		}	else {
+			$searchQuery = "SELECT * FROM tbl_grades_applicant WHERE fld_applicantID = $applicantID AND fld_subjectID = $subjectId";
+			$ret = mysqli_query($conn, $searchQuery);
+			$sub = mysqli_fetch_assoc($ret);
+			if ($sub) {
+				$id = $sub['id'];
+				$query = "UPDATE tbl_grades_applicant SET fld_applicantID = '$applicantID' , fld_subjectID = '$subjectId', fld_grade = '$grade' WHERE id = $id";
+			} else {
+				$query = "INSERT INTO tbl_grades_applicant (fld_applicantID, fld_subjectID, fld_grade) VALUES ('$applicantID', '$subjectId', '$grade')";	
+			}
+			
+		$test = mysqli_query($conn, $query);
+		}
+		
+	}
+	
+	$json['success'] = true;
+	$json['message'] = "Grades has been saved.";
+	echo json_encode($json, 200);
+	exit();
 ?>
