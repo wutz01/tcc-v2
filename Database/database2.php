ee61a595cfe4a427427c415dc327bfdf11ef2fa0
ben10nnyson <realben10nnyson>
2019-03-25 16:22:46 +0800

    db

--- Database/database2.php
+++ Database/database2.php
@@ -1,15 +1,15 @@
 <?php
 
 	$servername = 'localhost';
-	// if ($env === 'production') {
-	// 	$user = 'u984568706_admin';
-	// 	$pass = 'RxV6QRBmVVam';
-	// 	$db = 'u984568706_tcc';
-	// } else {
+	if ($env === 'production') {
+		$user = 'u984568706_admin';
+		$pass = 'RxV6QRBmVVam';
+		$db = 'u984568706_tcc';
+	} else {
 		$user = 'root';
 		$pass = '';
 		$db = 'tanauaud_tcc';
-	// }
+	}
 
 
 	$GLOBALS['conn'] = $conn = mysqli_connect($servername, $user, $pass, $db);
